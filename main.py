a5269ef7e81fd03ed9c9a2db4b845c2fdd28ca99
Sampath2703 <naniganji2703@gmail.com>
2026-06-05 23:53:09 +0530

    agentic ai project

--- main.py
+++ main.py
@@ -1,56 +1,53 @@
-from fastapi import FastAPI,Query
+from fastapi import FastAPI
+from pydantic import BaseModel
 from langchain_groq import ChatGroq
+from langchain.tools import tool
 import os
 import requests
 from dotenv import load_dotenv
-from langchain.agents import create_agent
-from langchain.tools import tool
 
 load_dotenv()
 app = FastAPI()
 
 openweather_api_key = os.getenv("OPENWEATHER_API_KEY")
 
-
-llm=ChatGroq(
-    api_key = os.getenv("GROQ_API_KEY"),
-    model = "llama-3.3-70b-versatile"
+llm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+    model="llama-3.3-70b-versatile"
 )
 
 @tool
-def get_temp_info(city:str):
-    """
-    this is to get the temperature details
-    """
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={openweather_api_key}")
-    data=response.json()
-    return data
+def get_temp_info(city: str):
+    response = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={openweather_api_key}&units=metric"
+    )
+    data = response.json()
 
-agent=create_agent(
-    model=llm,
-    tools=[get_temp_info]
-)
-
-@app.post("/")
-def home():
     return {
-        "msg":"backend Successfuly running"
+        "city": city,
+        "temp": data["main"]["temp"],
+        "weather": data["weather"][0]["description"]
     }
 
+class WeatherRequest(BaseModel):
+    city: str
+    question: str
+
+
+@app.get("/")
+def home():
+    return {"msg": "backend successfully running"}
 
 @app.post("/get_weather")
-def incoming_parametrs(
-    city:str = Query(...),
-    question:str = Query(...)
-):
-    result = agent.invoke({
-        "msg":[{
-            "role":"user",
-            "content":f"city:{city} question{question}"
-        }]
-    })
+def get_weather(data: WeatherRequest):
 
-    return {
-        "msg":result
-    }
-    
+    weather = get_temp_info.invoke(data.city)
+
+    answer = f"""
+City: {weather['city']}
+Temperature: {weather['temp']}°C
+Weather: {weather['weather']}
+User Question: {data.question}
+"""
+
+    return {"msg": answer}
