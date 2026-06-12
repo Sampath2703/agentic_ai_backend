a7c06da8b1f9f17385244ad76206cccdb8d111ab
Sampath2703 <naniganji2703@gmail.com>
2026-06-12 17:46:14 +0530

    errors

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
 app = FastAPI()
 
 openweather_api_key = os.getenv("OPENWEATHER_API_KEY")
@@ -19,37 +20,8 @@ llm = ChatGroq(
 @tool
 def get_temp_info(city: str):
     """
-    Get temperature, humidity, wind speed and weather condition for a city.
-    """
-    response = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={openweather_api_key}&units=metric"
-    )
-
-    data = response.json()
-
-    return {
-        "city": city,
-        "temp": data["main"]["temp"],
-        "humidity": data["main"]["humidity"],
-        "wind_speed": data["wind"]["speed"],
-        "weather": data["weather"][0]["description"]
-    }
-
-class WeatherRequest(BaseModel):
-    city: str
-    question: str
-
-
-@app.get("/")
-def home():
-    return {"msg": "backend successfully running"}
-
-@tool
-def get_temp_info(city: str):
-    """
     Get temperature, humidity, wind speed and weather condition for a city using OpenWeather API.
     """
-
     response = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={openweather_api_key}&units=metric"
     )
@@ -68,4 +40,44 @@ def get_temp_info(city: str):
         "humidity": data["main"]["humidity"],
         "wind_speed": data["wind"]["speed"],
         "weather": data["weather"][0]["description"]
+    }
+
+class WeatherRequest(BaseModel):
+    city: str
+    question: str
+
+@app.get("/")
+def home():
+    return {"msg": "backend successfully running"}
+
+@app.post("/get_weather")
+def get_weather(data: WeatherRequest):
+    weather = get_temp_info.invoke({"city": data.city})
+
+    if "error" in weather:
+        return {"error": weather["error"]}
+
+    prompt = f"""
+You are a weather assistant.
+
+Weather Data:
+City: {weather['city']}
+Temperature: {weather['temp']}°C
+Humidity: {weather['humidity']}%
+Wind Speed: {weather['wind_speed']} m/s
+Condition: {weather['weather']}
+
+User Question:
+{data.question}
+
+Give a short helpful answer based on weather conditions.
+"""
+
+    result = llm.invoke(prompt)
+
+    return {
+        "msg": {
+            **weather,
+            "answer": result.content
+        }
     }
